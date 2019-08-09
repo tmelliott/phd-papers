b47a12bba9d1e1b3ed8b59df64f889e20c00c795
Tom Elliott <tomelliottnz@gmail.com>
2019-08-09 12:02:37 +1200

    More edits

--- anzjs2018/figures/02_gtfs_delays.R
+++ anzjs2018/figures/02_gtfs_delays.R
@@ -6,7 +6,7 @@ s <- paste0("2019-01-01 12:",
         ":00") %>%
     as.POSIXct
 
-delay <- c(30, 60, 90)
+delay <- c(30, 60, 120)
 a <- s[1:3] + delay
 
 pdf('figures/02_gtfs_delays.pdf', width = 7, height = 2.5, onefile = FALSE)
@@ -30,26 +30,29 @@ egg::ggarrange(
         geom_point(aes(s[4] + delay, d[4]),
             shape = 21, fill = "white"),
     ggplot() +
+        geom_path(
+            aes(
+                c(s[2], a[2], a[2], a[3], a[3], a[3] + 60),
+                c(4.5, 3.5, 4, 1, 2, 1)
+            ),
+            lty = 2
+        ) +
         geom_point(
             aes(
                 c(s[2], a[-1]),
                 c(4.5, 4, 2)
-            )
-        ) +
-        geom_path(
-            aes(
-                c(s[2], a[2], a[2], a[3], a[3]),
-                c(4.5, 3.5, 4, 1.5, 2)
-                # as.integer(c(
-                #     s[4] - a[1], 
-                #     s[4] - a[1] - (a[2] - s[1]),
-                #     s[4] - a[2],
-                #     s[4] - a[2] - (a[3] - s[2]),
-                #     s[4] - a[3]
-                # ) / 60)
             ),
-            lty = 2
+            shape = 21, fill = "white"
         ) +
+        geom_text(
+            aes(
+                c(s[2], a[-1]), 
+                c(4.5, 4, 2),
+                label = paste0("ETA[", 1:3, "]")
+            ),
+            parse = TRUE,
+            nudge_y = 0.3, nudge_x = 10) +
+        ylim(0, 5) +
         xlab("Time") +
         ylab("ETA (minutes)") +
         theme_classic(),
