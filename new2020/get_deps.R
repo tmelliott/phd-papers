8ac874572712014e86e963184ec1ccea64a0cf7a
Tom Elliott <tomelliottnz@gmail.com>
2020-10-07 08:11:18 +1300

    Install if no dep file specified

--- new2020/get_deps.R
+++ new2020/get_deps.R
@@ -10,8 +10,10 @@ pkgs <- c(
     stringr::str_match(text, "([a-zA-Z0-9]+)::")[,2]
 )
 pkgs <- unique(pkgs[!is.na(pkgs)])
-if (length(ca)) saveRDS(pkgs, file = ca[1], version = 2)
-pkgs <- pkgs[!sapply(pkgs, requireNamespace, quietly = TRUE)]
-
-if (length(pkgs))
-    install.packages(pkgs)
+if (length(ca)) {
+    saveRDS(pkgs, file = ca[1], version = 2)
+} else {
+    pkgs <- pkgs[!sapply(pkgs, requireNamespace, quietly = TRUE)]
+    if (length(pkgs))
+        install.packages(pkgs)
+}
